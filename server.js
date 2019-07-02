240995eacbd46648b8df4b4f4a33a8d86e8f300b
Carlos Costa <carlosvini94@gmail.com>
2019-07-02 15:09:30 -0300

    Inicialização do Projeto

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+// Iniciando o App
+const app = express();
+
+//Inciando mongoose
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+    useNewUrlParser: true
+});
+requireDir('./src/models');
+
+// Rotas
+app.use('/api', require('./src/routers'));
+
+app.listen(3001);
